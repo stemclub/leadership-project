8e726578c13807f3b2b73e29a5955077a9fe6ee5
shalin <shalin@Shalin-PC>
2014-10-19 15:21:17 -0400

    ...Turns out that I have to keep writing the nextEventToBeDone. ugh.................................................................................................................

--- The Project - Leadership Program/src/services/ProgressInGame.java
+++ The Project - Leadership Program/src/services/ProgressInGame.java
@@ -1,11 +1,14 @@
 package services;
 
 public class ProgressInGame {	
-	private boolean[] tiersFinished = new boolean[4];
+	private boolean[] tiersFinished = new boolean[5];
 	private char[] tierOne = new char[1];
 	private char[] tierTwo = new char[3];
 	private char[] tierThree = new char[6];
 	private char[] tierFour = new char[6];
+	private char[] tierFive = new char[6];
+	
+	private char[][] tiers = {tierOne, tierTwo, tierThree, tierFour, tierFive};
 	
 	public byte isEventDone(int Event){
 		if(tiersFinished[Event] == true){
@@ -17,7 +20,7 @@ public class ProgressInGame {
 	}
 	
 	public void restartGame(){
-		for(int t = 0; t<=3; t++){
+		for(int t = 0; t<=4; t++){
 			tiersFinished[t] = false;
 		}
 		System.out.println("Restarting...[success]");
@@ -29,132 +32,154 @@ public class ProgressInGame {
 			return returning;
 		}
 		else if(tiersFinished[1] == false){
-			if(tierOne[0] == 'a'){
+			if(tiers[0][0] == 'a'){
 				int[] returning = {1,0};
 				return returning;
 			}
-			else if(tierOne[0] == 'b'){
+			else if(tiers[0][0] == 'b'){
 				int[] returning = {1,1};
 				return returning;
 			}
-			else if(tierOne[0] == 'c'){
+			else if(tiers[0][0] == 'c'){
 				int[] returning = {1,2};
 				return returning;
 			}
 		}
 		else if(tiersFinished[2] == false){
-			if(tierTwo[0] == 'a'){
+			if(tiers[1][0] == 'a'){
 				int[] returning = {2,0};
 				return returning;
 			}
-			else if(tierTwo[0] == 'b'){
+			else if(tiers[1][0] == 'b'){
 				int[] returning = {2,1};
 				return returning;
 			}
-			else if(tierTwo[0] == 'c'){
+			else if(tiers[1][0] == 'c'){
 				int[] returning = {2,2};
 				return returning;
 			}
-			else if(tierTwo[1] == 'a'){
+			else if(tiers[1][1] == 'a'){
 				int[] returning = {2,2};
 				return returning;
 			}
-			else if(tierTwo[1] == 'b'){
+			else if(tiers[1][1] == 'b'){
 				int[] returning = {2,3};
 				return returning;
 			}
-			else if(tierTwo[1] == 'c'){
+			else if(tiers[1][1] == 'c'){
 				int[] returning = {2,4};
 				return returning;
 			}
-			else if(tierTwo[2] == 'a'){
+			else if(tiers[1][2] == 'a'){
 				int[] returning = {2,4};
 				return returning;
 			}
-			else if(tierTwo[2] == 'b'){
+			else if(tiers[1][2] == 'b'){
 				int[] returning = {2,5};
 				return returning;
 			}
-			else if(tierTwo[2] == 'c'){
+			else if(tiers[1][2] == 'c'){
 				int[] returning = {2,0};
 				return returning;
 			}
 		}
 		else if(tiersFinished[3] == false){
-			if(tierThree[0] == 'a'){
+			if(tiers[2][0] == 'a'){
 				int[] returning = {3,0};
 				return returning;
 			}
-			else if(tierThree[0] == 'b'){
+			else if(tiers[2][0] == 'b'){
 				int[] returning = {3,1};
 				return returning;
 			}
-			else if(tierThree[0] == 'c'){
+			else if(tiers[2][0] == 'c'){
 				int[] returning = {3,2};
 				return returning;
 			}
-			if(tierThree[1] == 'a'){
+			if(tiers[2][1] == 'a'){
 				int[] returning = {3,1};
 				return returning;
 			}
-			else if(tierThree[1] == 'b'){
+			else if(tiers[2][1] == 'b'){
 				int[] returning = {3,3};
 				return returning;
 			}
-			else if(tierThree[1] == 'c'){
+			else if(tiers[2][1] == 'c'){
 				int[] returning = {3,2};
 				return returning;
 			}
-			else if(tierThree[2] == 'a'){
+			else if(tiers[2][2] == 'a'){
 				int[] returning = {3,3};
 				return returning;
 			}
-			else if(tierThree[2] == 'b'){
+			else if(tiers[2][2] == 'b'){
 				int[] returning = {3,2};
 				return returning;
 			}
-			else if(tierThree[2] == 'c'){
+			else if(tiers[2][2] == 'c'){
 				int[] returning = {3,1};
 				return returning;
 			}
-			else if(tierThree[3] == 'a'){
+			else if(tiers[2][3] == 'a'){
 				int[] returning = {3,1};
 				return returning;
 			}
-			else if(tierThree[3] == 'b'){
+			else if(tiers[2][3] == 'b'){
 				int[] returning = {3,4};
 				return returning;
 			}
-			else if(tierThree[3] == 'c'){
+			else if(tiers[2][3] == 'c'){
 				int[] returning = {3,3};
 				return returning;
 			}
-			else if(tierThree[4] == 'a'){
+			else if(tiers[2][4] == 'a'){
 				int[] returning = {3,1};
 				return returning;
 			}
-			else if(tierThree[4] == 'b'){
+			else if(tiers[2][4] == 'b'){
 				int[] returning = {3,3};
 				return returning;
 			}
-			else if(tierThree[4] == 'c'){
+			else if(tiers[2][4] == 'c'){
 				int[] returning = {3,2};
 				return returning;
 			}
-			else if(tierThree[5] == 'a'){
+			else if(tiers[2][5] == 'a'){
 				int[] returning = {3,5};
 				return returning;
 			}
-			else if(tierThree[5] == 'b'){
+			else if(tiers[2][5] == 'b'){
 				int[] returning = {3,2};
 				return returning;
 			}
-			else if(tierThree[5] == 'c'){
+			else if(tiers[2][5] == 'c'){
 				int[] returning = {3,3};
 				return returning;
 			}
 		}
-		int[] returning = {4, 0};
-		return returning;
+		else if(tiersFinished[4] == false){
+			if(tiers[3][0] == 'a'){
+				int[] returning = {4,0};
+				return returning;
+			}
+			else if(tiers[3][0] == 'b'){
+				int[] returning = {4,1};
+				return returning;
+			}
+			else if(tiers[3][0] == 'c'){
+				int[] returning = {4,2};
+				return returning;
+			}
+			else if(tiers[3][1] == 'a'){
+				
+			}
+		}
+	}
+	
+	public void totalScore(ProgressInGame n){
+		int[] compare = n.nextEventToBeDone();
+		if(compare[0] == 4 && compare[1] == 0){
+			
+		}
 	}
 }
