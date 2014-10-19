fd3e28aceea834f0e49435863711e04f54131021
stemclub <gwstemclub@gmail.com>
2014-10-19 18:07:56 -0400

    Adding some more cases

--- The Project - Leadership Program/src/services/ProgressInGame.java
+++ The Project - Leadership Program/src/services/ProgressInGame.java
@@ -171,8 +171,65 @@ public class ProgressInGame {
 				return returning;
 			}
 			else if(tiers[3][1] == 'a'){
-				
+				int[] returning = {4,1};
+				return returning;
 			}
+			else if(tiers[3][1] == 'b'){
+				int[] returning = {4,2};
+				return returning;
+			}
+			else if(tiers[3][1] == 'c'){
+				int[] returning = {4,3};
+				return returning;
+			}
+			else if(tiers[3][2] == 'a'){
+				int[] returning = {4,2};
+				return returning;
+			}
+			else if(tiers[3][2] == 'b'){
+				int[] returning = {4,3};
+				return returning;
+			}
+			/*else if(tiers[3][2] == 'c'){
+				int[] returning = {4,3};
+				return returning;
+			}
+			else if(tiers[3][3] == 'a'){
+				int[] returning = {4,3};
+				return returning;
+			}
+			else if(tiers[3][3] == 'b'){
+				int[] returning = {4,3};
+				return returning;
+			}
+			else if(tiers[3][3] == 'c'){
+				int[] returning = {4,3};
+				return returning;
+			}
+			else if(tiers[3][4] == 'a'){
+				int[] returning = {4,3};
+				return returning;
+			}
+			else if(tiers[3][4] == 'b'){
+				int[] returning = {4,3};
+				return returning;
+			}
+			else if(tiers[3][4] == 'c'){
+				int[] returning = {4,3};
+				return returning;
+			}
+			else if(tiers[3][5] == 'a'){
+				int[] returning = {4,3};
+				return returning;
+			}
+			else if(tiers[3][5] == 'b'){
+				int[] returning = {4,3};
+				return returning;
+			}
+			else if(tiers[3][5] == 'c'){
+				int[] returning = {4,3};
+				return returning;
+			}*/
 		}
 	}
 	
