0fef4a965f4ce089955dbf2aa3f2f6da700e72a7
stemclub <gwstemclub@gmail.com>
2014-10-26 16:00:19 -0400

    Made some more comments

--- The Project - Leadership Program/src/objects/DataStorage.java
+++ The Project - Leadership Program/src/objects/DataStorage.java
@@ -3,14 +3,17 @@ package objects;
 public class DataStorage {
 	public static int scores[][][] = {
 		{
+            //Tier One
 			{1,2,3},
 		},
 		{
+            //Tier Two
 			{2,1,3},
 			{3,2,1},
 			{1,3,2},
 		},	
 		{
+            //Tier Three
 			{2,3,4},
 			{3,1,2},
 			{1,2,3},
@@ -19,6 +22,7 @@ public class DataStorage {
 			{3,2,1},
 		}, 
 		{
+            //Tier Four
 			{1,2,3},
 			{2,3,1},
 			{3,2,1},
@@ -27,6 +31,7 @@ public class DataStorage {
 			{3,1,2},
 		},
 		{
+            //Tier Five
 			{1,3,2},
 			{2,3,1},
 			{3,2,1},
