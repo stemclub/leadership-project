c76f22dc5629c02212856ced7f44624e63bfd128
shalin <shalin@Shalin-PC>
2014-10-25 20:40:00 -0400

    Making progress on Application

--- The Project - Leadership Program/src/objects/Event.java
+++ The Project - Leadership Program/src/objects/Event.java
@@ -4,11 +4,13 @@ public class Event {
 	private String[] videoPaths;
 	private char[] choices;
 	private int scoreForRound;
+	private int tier;
+	private int eventNumber;
 	
 	private void makeScore(){
 		for(int x = 0; x <= choices.length; x++){
 			if(!(choices[x] == 'z')){
-				
+				scoreForRound = DataStorage.scores[tier][eventNumber][x];
 			}
 		}
 	}
