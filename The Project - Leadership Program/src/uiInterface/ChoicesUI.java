192fce9870f7c9a1e35c2d8837412b64061dd077
stemclub <gwstemclub@gmail.com>
2014-11-01 19:29:09 -0400

    Updating the UI

--- The Project - Leadership Program/src/uiInterface/ChoicesUI.java
+++ The Project - Leadership Program/src/uiInterface/ChoicesUI.java
@@ -1,14 +1,42 @@
 package uiInterface;
 
-import javax.swing.JButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ChoicesUI {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class ChoicesUI extends JFrame{
 	private JButton Choice1 = new JButton("Choice 1");
 	private JButton Choice2 = new JButton("Choice 2");
 	private JButton Choice3 = new JButton("Choice 3");
 	private JButton ReplayVideo = new JButton("Replay the Video");
+	public JPanel ChoicesPanel = new JPanel();
+	public static char choice;
+	public static boolean replayVideo;
 	
 	public ChoicesUI(){
+		ChoicesPanel.add(Choice1);
 		
+	}
+	private class ButtonListener implements ActionListener{
+		public void actionPerformed(ActionEvent arg0) {
+			if(arg0.getSource() == Choice1){
+				choice = 'a';
+				replayVideo = false;
+			}
+			else if(arg0.getSource() == Choice2){
+				choice = 'b';
+				replayVideo = false;
+			}
+			else if(arg0.getSource() == Choice3){
+				choice = 'c';
+				replayVideo = false;
+			}
+			else if(arg0.getSource() == ReplayVideo){
+				replayVideo = true;
+			}
+		}
 	}
 }
