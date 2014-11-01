393690a7a87efe139f8bb56d0ae45feedde8395c
stemclub <gwstemclub@gmail.com>
2014-11-01 18:53:26 -0400

    Working on changes

--- The Project - Leadership Program/src/uiInterface/ChoicesUI.java
+++ The Project - Leadership Program/src/uiInterface/ChoicesUI.java
@@ -6,6 +6,9 @@ public class ChoicesUI {
 	private JButton Choice1 = new JButton("Choice 1");
 	private JButton Choice2 = new JButton("Choice 2");
 	private JButton Choice3 = new JButton("Choice 3");
+	private JButton ReplayVideo = new JButton("Replay the Video");
 	
-	
+	public ChoicesUI(){
+		
+	}
 }
