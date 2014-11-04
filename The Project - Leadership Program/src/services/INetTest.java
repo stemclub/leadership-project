f6f23f7b6c4d536a1b02a0a958ba3f25c81ae355
stemclub <gwstemclub@gmail.com>
2014-11-03 22:44:03 -0500

    Updated INetTest to be more reliable.

--- The Project - Leadership Program/src/services/INetTest.java
+++ The Project - Leadership Program/src/services/INetTest.java
@@ -1,21 +1,30 @@
 package services;
 
-import java.net.InetAddress;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.net.UnknownHostException;
 
 public class INetTest {
 	public static boolean main()
     {
-        try {
+		try {
+            //make a URL to a known source
+            URL url = new URL("http://www.google.com");
 
-            InetAddress address = InetAddress.getByName("java.sun.com");
+            //open a connection to that source
+            HttpURLConnection urlConnect = (HttpURLConnection)url.openConnection();
 
-            if(address == null)
-            {
-                return false;
-            }
+            //trying to retrieve data from the source. If there
+            //is no connection, this line will fail
+            Object objData = urlConnect.getContent();
 
         } catch (UnknownHostException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return false;
+        }
+        catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
             return false;
