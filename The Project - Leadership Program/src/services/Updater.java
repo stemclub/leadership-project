001a25b51eec3c76da4cff5922e43a0bc778238b
stemclub <gwstemclub@gmail.com>
2014-11-04 20:11:30 -0500

    More Verbosity

--- The Project - Leadership Program/src/services/Updater.java
+++ The Project - Leadership Program/src/services/Updater.java
@@ -12,10 +12,12 @@ public class Updater {
 		DownloadVideo down = new DownloadVideo();
 		if (!down.checkFolder()) {
 			if (!INetTest.main()) {
+				System.out.println("ERR: No internet connection and must fetch video");
 				JOptionPane.showMessageDialog(null, "Videos have to be downloaded and you do not appear to be connected to the internet. Please click OK, connect to the internet, and then reopen LeadItUp.", "Network Error", JOptionPane.PLAIN_MESSAGE);
 				return false;
 			} else {
 				if (!update()) {
+					System.out.println("WARN: Update fail. Continuing still possible");
 					JOptionPane.showMessageDialog(null, "Update failed. You can still continue using LeadItUp though.", "Warning", JOptionPane.PLAIN_MESSAGE);
 				}
 				if (down.main()) {
@@ -26,10 +28,12 @@ public class Updater {
 			}
 		} else {
 			if (!INetTest.main()) {
+				System.out.println("WARN: No internet. Continuing still possible");
 				JOptionPane.showMessageDialog(null, "You do not appear to be connected to the internet. Updates could not be checked and some functionalities may be limited.", "Warning", JOptionPane.PLAIN_MESSAGE);
 				return true;
 			} else {
 				if (!update()) {
+					System.out.println("WARN: Update fail. Continuing still possible");
 					JOptionPane.showMessageDialog(null, "Update failed. You can still continue using LeadItUp though.", "Warning", JOptionPane.PLAIN_MESSAGE);
 				}
 				return true;
