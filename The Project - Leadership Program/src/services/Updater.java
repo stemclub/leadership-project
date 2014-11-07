9a41e4c56eb4116a75015e84bba8a9ffac1abfc8
stemclub <gwstemclub@gmail.com>
2014-11-07 15:58:11 -0500

    Finished first iteration of updater class

--- The Project - Leadership Program/src/services/Updater.java
+++ The Project - Leadership Program/src/services/Updater.java
@@ -1,14 +1,40 @@
 package services;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 import javax.swing.JOptionPane;
 
 public class Updater {
 	public boolean update() {
 		System.out.print("Checking for updates to LeadItUp... ");
-		System.out.println("[Update class unfinished]");
-		return true;
+		String version = "0.1 Alpha";
+		//System.out.println(version);
+		try {
+			// Create a URL for the desired page
+        	URL url = new URL("https://raw.githubusercontent.com/stemclub/LeadItUp_deploy/master/version.info");       
+        
+        	// Read all the text returned by the server
+        	BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+        	String str = in.readLine().toString();
+        	//System.out.println(str);
+        	if (str.equals(version)) {
+        		System.out.println("[No updates]");
+        		in.close();
+        		return true;
+        	} else {
+        		String str2 = in.readLine().toString();
+            	System.out.print("[Update found]");
+            	JOptionPane.showMessageDialog(null, "Update found! Get version " + str + " from " + str2, "Update", JOptionPane.PLAIN_MESSAGE);
+            	in.close();
+            	return true;
+        	}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			return false;
+		}
 	}
 	public boolean main() throws IOException {
 		DownloadVideo down = new DownloadVideo();
